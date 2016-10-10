daf0302928b52437aa0dd99e2951be99fe9e8dd0
Kristi Heredia <froglander@dukworld.net>
2016-10-09 23:14:18 -0400

    Update schema file

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,5 @@ CREATE TABLE `burgers_db`.`burgers` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `burger_name` VARCHAR(255) NOT NULL,
   `devoured` TINYINT(1) NULL,
-  `date` TIMESTAMP NULL,
+  `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`));
-
-
-
