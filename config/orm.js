36476bcf14f55a723e7948965d338958b6b54513
Kristi Heredia <froglander@dukworld.net>
2016-10-09 22:07:02 -0400

    Basic functionality set up

---
+++ config/orm.js
@@ -0,0 +1,19 @@
+var connection = require('./connection.js');
+
+var orm = {
+	selectAll: function(tableInput, cb) {
+		var queryString = 'SELECT * FROM ' + tableInput + ';';
+		connection.query(queryString, function(err, result) {
+			if(err) throw err;
+			cb(result);
+		});
+	},
+	insertOne: function(tableInput, colInput) {
+
+	},
+	updateOne: function(tableInput) {
+
+	}
+};
+
+module.exports = orm;
---
+++ models/burger.js
@@ -0,0 +1,26 @@
+/*
+Here is where you set up a model for how to interface with the database
+*/
+
+var orm = require('../config/orm.js');
+
+
+var burger = {
+	all: function (cb) {
+		orm.selectAll('burgers', function(res) {
+			cb(res);
+		});
+	},
+	create: function(cols, vals, cb) {
+		orm.create('burgers', cols, vals, function (res) {
+			cb(res);
+		});
+	},
+	update: function(objColVals, condition, cb) {
+		orm.update('burgers', objColVals, condition, function(res) {
+			cb(res);
+		});
+	}
+};
+
+module.exports = burger;
